1ae5c1aa1d025cacee91e16c73aaafa4c03326fe
Fabián Valverde <110683318+fabianvalverde@users.noreply.github.com>
2023-04-04 19:22:23 -0600

    a

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using LibGit2Sharp;
+
+Console.WriteLine("Hello, World!");
+
+// using (var repo = new Repository(@"D:\source\LibGit2Sharp"))
+// {
+//     Commit commit = repo.Lookup<Commit>("73b48894238c3e9c37f9f3a696bbd4bffcf45ce5");
+//     Console.WriteLine("Author: {0}", commit.Author.Name);
+//     Console.WriteLine("Message: {0}", commit.MessageShort);
+// }
